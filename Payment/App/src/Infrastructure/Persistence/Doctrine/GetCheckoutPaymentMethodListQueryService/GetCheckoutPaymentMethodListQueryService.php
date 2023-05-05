188dcc2d27bbf18422bfd0b41ffb0a7b132c983e
szabolaszlo <laszlo.szabo@shoprenter.hu>
2023-05-05 16:21:38 +0200

    PaymentMethod

--- Payment/App/src/Infrastructure/Persistence/Doctrine/GetCheckoutPaymentMethodListQueryService/GetCheckoutPaymentMethodListQueryService.php
+++ Payment/App/src/Infrastructure/Persistence/Doctrine/GetCheckoutPaymentMethodListQueryService/GetCheckoutPaymentMethodListQueryService.php
@@ -8,6 +8,11 @@ use App\Domain\PaymentMethodId;
 use App\Domain\ShippingMethodId;
 use Doctrine\ORM\EntityManagerInterface;
 
+/**
+ * Azért lett egy ilyen query service mert bizonyos feltételek alapjá szűrőgetni kell
+ * és azt mindet be lehet ide verni
+ * Persze a specifikaction pattern is megoldás lehetne ha időmilliomosak vagyunk
+ */
 class GetCheckoutPaymentMethodListQueryService implements QueryServiceInterface
 {
     private $entityManager;
@@ -23,8 +28,9 @@ class GetCheckoutPaymentMethodListQueryService implements QueryServiceInterface
             ->entityManager
             ->getRepository(PaymentMethod::class)
             ->findAll();
+        // Azért findAll mert ha rossz ID jön akkor nincs lista és hadd működjön
 
-        //Filter payment methods by shipping method id
+        // Ide jöhetne akár adatbázis szinten hogy ShippingId szerint szűrjönk és azokat is ejtsük ki amelyek nincsenek engedélyezve
 
         return $paymentMethods;
     }
