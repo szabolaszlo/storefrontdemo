e060a44dcd8446327d223f729cc94710db255778
Eszter Pálmai <eszter.palmai@shoprenter.hu>
2023-05-05 13:56:07 +0200

    create payment initiation use case skeleton

---
+++ Payment/App/src/Presentation/Api/Controller/InitiatePaymentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Presentation\Api\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class InitiatePaymentController extends AbstractController
+{
+    public function index() {
+
+    }
+
+}
